fcf676767f199ddb9daedb335d81240e2417c8ec
freedom-1 <ushohabbos@gmail.com>
2023-10-19 09:45:26 +0500

    first commit

---
+++ lesson2.py
@@ -0,0 +1,60 @@
+# Variables
+# x=2; #int tipidagi o'zgaruvchi
+# print(x);
+# print(type(x)); #O'zgaruvchilar tipini aniqlash
+# X=4;
+# print(X);
+#int float char string bool
+# x=eval(input())
+# print(type(x))
+# z=1.25
+# z1=15
+# print(type(z1*z))
+# go=8
+# home=9
+# #Concatenation(Konketenatsiya)
+# print(f"{go}+{home}={go+home}");
+# print(go,"*",home,"=");
+# print(f"{go}-{home}={go-home}");
+# print(f"{go}/{home}={go/home}");
+
+# s1="Assalomu"
+# s2="alaykum"
+# s3='do\'stlar'
+# # s3="%s %s"%(s1,s2)
+# s4 = "{} {} {}".format(s1, s2, s3)
+# print(s4)
+
+#Kompleks sonlar bilan ishlash
+#a+ib
+#x^2=-5; x=?
+# x1=complex(1,2)
+# x2=complex(-1,2)
+# print(x1*x2)
+
+# go=63
+# print(bin(go)) #ikkilik
+# print(oct(go)) #sakkizlik
+# print(hex(go)) #O'n oltilik
+
+# go=45
+# home=9
+# print(f"{bin(go)} + {bin(home)} = {bin(int(go+home))}")
+# print(f"{bin(go)} - {bin(home)} = {bin(int(go-home))}")
+# print(f"{bin(go)} * {bin(home)} = {bin(int(go*home))}")
+# print(f"{bin(go)} / {bin(home)} = {bin(int(go/home))}")
+# print(type(go/home)) #Float dan ikkilikka o'tib bo'lmaydi
+
+# ASCII kodi bilan ishlash chr(int)
+# belgi = 'x'
+# print(ord(belgi))
+# X ning ASCII kodi  88
+# print(chr(88))
+
+
+
+
+
+
+
+
